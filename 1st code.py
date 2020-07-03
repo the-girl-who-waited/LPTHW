0524f0c6c9d888f232aad6cbc2bc9ef1b064741f
thegirlwhowaited <trueluv4evr27@gmail.com>
2020-07-03 15:18:45 -0500

    first commit message

---
+++ 1st code.py
@@ -0,0 +1,12 @@
+print("NEVER listen to boy bands while coding")
+x = 14
+print("not only do you feel"+str(x))
+print("you start turning their name into cpde")
+name = "nsync"
+print(name)
+print("Backstreet boys")
+print("o-town")
+print("the choices are endless in 90s teeny bopper music")
+print("this ladies and gentleman is a successful first code")
+print("BOOYAH!")
+
